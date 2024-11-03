398b7caeb4c012b4cc776b6491e197aea794ca5d
siroo2137 <72565664+siroo2137@users.noreply.github.com>
2024-11-03 18:43:30 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 cd /home/container
 
-# allow execution of the proxy
-chmod +x infrared
-
 # Replace Startup Variables
 MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
 echo ":/home/container$ ${MODIFIED_STARTUP}"
