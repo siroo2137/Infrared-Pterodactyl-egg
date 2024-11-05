fbbba01e45b4d2d94d3f9c289f4b141df4955bf7
siroo2137 <72565664+siroo2137@users.noreply.github.com>
2024-11-05 22:34:18 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,64 +3,58 @@ cd /home/container
 
 # Replace Startup Variables
 MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+# echo "${MODIFIED_STARTUP}"
 
-# Prepare the proxies
+echo """
+debug: false
+receiveProxyProtocol: false
+useRedisConfigs: false
+underAttack: false
+connectionThreshold: 50
+trackBandwidth: false
+prometheus:
+  enabled: false
+  bind: :9070
+api:
+  enabled: true
+  bind: :65534
+mojangAPIenabled: false
+geoip:
+  enabled: false
+  databaseFile: GeoLite2-Country.mmdb
+  enableIprisk: false
+redis:
+  host: 172.18.0.1
+  pass:
+  db: 0
+configredis:
+  host: 172.18.0.1
+  pass:
+  db: 0
+rejoinMessage: §2Polacz sie ponownie aby sie zweryfikowac.
+blockedMessage: §cTwoj adres ip zostal zablokowany ze wzgledu na podejrzany ruch!
+genericJoinResponse: §3§lCataclysmProxy\n\n§cTen serwer nie istnieje lub zmienil adres IP!\n\n§9dc.cataclysm.su
+genericping:
+  version: CataclysmProxy
+  description: Ten serwer nie istnieje lub zmienil adres IP!
+  iconPath: 
+tableflip:
+  enabled: false
+  pidfile: infrared.pid
+  """ > config.yml
 
 # check if the folder proxies doesn't exists
-if [ ! -d "/home/container/proxies" ]; then
+if [ ! -d "/home/container/configs" ]; then
     # create the folder
-    mkdir /home/container/proxies
+    mkdir /home/container/configs
 fi
 
-function createProxyYml {
-
-    local proxyNumber=$1
-    local domains="DOMAINS_${proxyNumber}"
-    local addresses="ADDRESSES_${proxyNumber}"
-
-    # Check if the env variable is set
-    if [ -z "${!domains}" ]; then
-        echo "The domains variable is not set. Please set it to the $1 domain of the proxy"
-        exit 1
-    fi
-
-    # Check if the env variable is set
-    if [ -z "${!addresses}" ]; then
-        echo "The addresses env variable is not set. Please set it to the $1 domain of the proxy"
-        exit 1
-    fi
-
-    # If file exists, delete it
-    if [ -f "/home/container/proxies/$1.yml" ]; then
-        rm /home/container/proxies/$1.yml
-    fi
-
-    # Create the file
-    echo "domains:" >"/home/container/proxies/$1.yml"
-    echo "${!domains}" | while IFS=',' read -r domain; do
-        echo "  - \"$domain\"" >>"/home/container/proxies/$1.yml"
-    done
-
-    echo "addresses:" >>"/home/container/proxies/$1.yml"
-    echo "${!addresses}" | while IFS=',' read -r address; do
-        echo "  - $address" >>"/home/container/proxies/$1.yml"
-    done
+echo """
+{
+    "domainNames": ["localhost"],
+    "proxyTo": "127.0.0.1:25565"
 }
-
-# Check if PROXY_COUNT is set if not assume the User configured everything manually
-if [ -n "$PROXY_COUNT" ]; then
-    # Check if it's a number
-    if ! [[ $PROXY_COUNT =~ ^[0-9]+$ ]]; then
-        echo "The PROXY_COUNT env variable is not a number"
-        exit 1
-    fi
-
-    # Loop through the number of proxies and create the yml files
-    for i in $(seq 1 $PROXY_COUNT); do
-        createProxyYml $i
-    done
-fi
+""" > configs/temp.json
 
 # Run the Server
 ${MODIFIED_STARTUP}
